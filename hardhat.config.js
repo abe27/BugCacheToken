bce4ace751f4a645a7640c515e4676b1c820ff74
Taweechai Yuenyang <krumii.it@gmail.com>
2021-09-03 15:06:40 +0700

    update bsc network

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,18 +20,44 @@ const { API_KEY, PRIVATE_KEY } = require('./secrets.json');
  */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "localhost",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${API_KEY}`,
       accounts: [`0x${PRIVATE_KEY}`]
     },
     ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${API_KEY}`,
+      url: `https://ropsten.infura.io/v3/${API_KEY}`,
       accounts: [`0x${PRIVATE_KEY}`],
-    },   
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
     goerli: {
       url: `https://goerli.infura.io/v3/${API_KEY}`,
       accounts: [`0x${PRIVATE_KEY}`]
+    },
+    moonbase: {
+      url: `https://rpc.testnet.moonbeam.network`,
+      chainId: 1287,
+      gasPrice: 20000000000,
+      accounts: [PRIVATE_KEY]
+    },
+    bsctest: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: PRIVATE_KEY}
+    },
+    bscmain: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: PRIVATE_KEY}
     }
   }
 };
