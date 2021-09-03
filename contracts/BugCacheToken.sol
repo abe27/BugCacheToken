60696c4c839a67318e2136d83af42a2bf470352c
Taweechai Yuenyang <krumii.it@gmail.com>
2021-09-03 10:46:49 +0700

    test and deploy

---
+++ contracts/BugCacheToken.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4 <0.9.0;
+
+import "hardhat/console.sol";
+
+contract BugCacheToken {
+    string public name = "Bug Cache Token";
+    string public symbol = "BCT";
+    uint256 public decimal = 10;
+    uint256 public totalSupply = 10000000000000 * decimal;
+    address public owner;
+    mapping(address => uint256) balances;
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        console.log("Sender balance is %s tokens", balances[msg.sender]);
+        console.log("Trying to send %s tokens to %s", amount, to);
+
+        require(balances[msg.sender] >= amount, "Not enough tokens!");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
